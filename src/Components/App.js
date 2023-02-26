373d4069704ea9524df76906bf981b45b80ff006
pallab17 <91554568+pallab17@users.noreply.github.com>
2023-02-27 02:00:51 +0530

    added files

---
+++ src/Components/App.js
@@ -0,0 +1,30 @@
+import React  from "react";
+import Header from "./Header";
+import "../styles.css";
+import Footer from "./Footer";
+import Note from "./Note";
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Note/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
+
+//1. Create a new React app. done
+//2. Create a App.jsx component. done
+//3. Create a Header.jsx component that renders a <header> element
+//to show the Keeper App name in an <h1>. done 
+//4. Create a Footer.jsx component that renders a <footer> element
+//to show a copyright message in a <p> with a dynamically updated year.
+//5. Create a Note.jsx component to show a <div> element with a
+//<h1> for a title and a <p> for the content.
+//6. Make sure that the final website is styled like the example shown here:
+//https://l1pp6.csb.app/
+
+//HINT: You will need to study the classes in teh styles.css file to appy styling.
---
+++ src/Components/Note.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Note(){
+    return <div className="note">
+        <h1 >This is Note title</h1>
+        <p  >This is Note Content</p>
+    </div>
+
+}
+
+export default Note;
